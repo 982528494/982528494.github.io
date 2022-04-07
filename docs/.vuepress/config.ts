0fe9938adc1fd7c7122609b2a100c11bebe55780
dengpc <982528494@qq.com>
2022-04-07 17:50:05 +0800

    update

--- docs/.vuepress/config.ts
+++ docs/.vuepress/config.ts
@@ -17,7 +17,7 @@ export default defineConfig4CustomTheme<VdoingThemeConfig>({
         "Sky's blog,专注web前端学习与总结。JavaScript,ES6,TypeScript,vue,React,css3,html5,Node,git,github等技术文章。",
     },
   },
-  // base: '/', // 默认'/'。如果你想将你的网站部署到如 https://foo.github.io/bar/，那么 base 应该被设置成 "/bar/",（否则页面将失去样式等文件）
+  base: "/Blog/",
 
   // 主题配置
   themeConfig: {
